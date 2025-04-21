52ee94db404e03a2f70c7d3f9bbf19c176412d1d
kasaranenikarthikeya <149353084+kasaranenikarthikeya@users.noreply.github.com>
2025-04-22 00:01:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import "./styles.css";
 
 // Dynamically set the API base URL for local and deployed environments
-window.API_BASE_URL = process.env.REACT_APP_API_URL || "https://ai-resume-builder-3lej.onrender.com";
+window.API_BASE_URL = process.env.REACT_APP_API_URL || "https://ai-resume-maker-gjv8.onrender.com";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
